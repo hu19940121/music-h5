3242563f9222a0c87c1c91e2b9a372e127ba21e0
kaier <673822024@qq.com>
2020-11-15 19:34:43 +0800

    去除预渲染 有bug

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -70,13 +70,13 @@ export default {
       show: false
     }
   },
-  metaInfo: {
-    title: '云音乐(破解，vip，网易云)',
-    meta: [
-      { name: 'Keywords', content: '免费，破解，vip免费，网易云音乐，音乐，播放器，网易，下载，播放，DJ，免费，明星，精选，歌单，识别音乐，收藏，分享音乐，音乐互动，高音质，320K，音乐社交，官网，music.kaier001.com' },
-      { name: 'description', content: '云音乐是一款专注于发现与分享的音乐产品，依托专业音乐人、DJ、好友推荐及社交功能，为用户打造全新的音乐生活。' }
-    ]
-  },
+  // metaInfo: {
+  //   title: '云音乐(破解，vip，网易云)',
+  //   meta: [
+  //     { name: 'Keywords', content: '免费，破解，vip免费，网易云音乐，音乐，播放器，网易，下载，播放，DJ，免费，明星，精选，歌单，识别音乐，收藏，分享音乐，音乐互动，高音质，320K，音乐社交，官网，music.kaier001.com' },
+  //     { name: 'description', content: '云音乐是一款专注于发现与分享的音乐产品，依托专业音乐人、DJ、好友推荐及社交功能，为用户打造全新的音乐生活。' }
+  //   ]
+  // },
   mounted() {
     window.scanCallback = function(data) {
       console.log('扫码结果---------' + JSON.stringify(data))
